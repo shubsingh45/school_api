1e1db850f66dba0dd1d0a69943e9de3159d7732b
shubsingh45 <157453436+shubsingh45@users.noreply.github.com>
2024-08-23 11:59:57 +0530

    Update UserRoutes.js

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { AddSchool, ListOfSchool } from '../controllers/SchoolController.js'
+import { AddSchool, ListOfSchool, Test } from '../controllers/SchoolController.js'
 
 
 const router = express.Router()
 router.post('/addSchool', AddSchool)
 router.get('/listSchools', ListOfSchool)
-
+router.get('/', Test)
 export default router
